40aa3a30cfd89302408fc03f64011e38e7104bec
jmternes <68615887+jmternes@users.noreply.github.com>
2022-05-10 12:49:02 -0500

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+var X = `String here... \"It's a strange one\", but it's not too weird, right?`; //Created a variable called X with a value of that long string
+
+window.alert(X); //This brings a pop-up window when the page loads, printing X
+
+var Y = "John" + " Michael" //Creates a variable called Y with string value "John Michael"
+
+document.write(Y); //This prints the variable Y on the page
+
+var bball = "LeBron", soccer = "Messi", football = "Brady"; //Creates multiple variables at once, with different string values attached
+
+document.write(bball); //This prints the variable bball on page
+
+var sum = 3+5; //variable called sum with value of 3+5
+
+document.write(sum); //prints the variable sum (8)
