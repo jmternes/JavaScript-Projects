3855a2839adbb96fe9962ec84629dc08231e198a
jmternes <68615887+jmternes@users.noreply.github.com>
2022-05-10 14:36:00 -0500

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function secondFunction () { //Creating a function called secondFunction
+    var sentence = "JavaScript"; //Creating a variable called sentence with string value "JavaScript"
+    sentence += " is a bit tricky at first!"; //+= Operator to concatenate the two strings
+    document.getElementById("Concatenate").innerHTML = sentence; //Returns an element with same ID as the paragraph element in HTML file
+}
