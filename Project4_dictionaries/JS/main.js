e47293299f53325b4728b2053d3712c002cb27e1
jmternes <68615887+jmternes@users.noreply.github.com>
2022-05-10 16:41:01 -0500

    Add files via upload

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function myDictionary() {
+    var Lebron = {
+        Height:"6-9",
+        Weight:"260 pounds",
+        Team:"Lakers",
+        Number:6,
+        Hometown:"Akron"
+    };
+    document.getElementById("Dictionary").innerHTML = Lebron.Height;
+}
