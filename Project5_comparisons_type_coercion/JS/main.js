1e5add6639d694760dee99416bdbe50b747ca6f1
jmternes <68615887+jmternes@users.noreply.github.com>
2022-05-17 13:03:18 -0500

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,11 @@
+document.write(typeof 500);
+
+document.write("1000" + 6);
+
+function infinity() {
+document.getElementById("large").innerHTML = 5E399;
+}
+
+function negInfinity() {
+    document.getElementById("small").innerHTML = -5E400
+}
